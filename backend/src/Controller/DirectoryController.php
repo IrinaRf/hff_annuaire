e83b9337a512c4639473435048dc8b86887b3131
nekena RABEHARIVONY <nekena@fraise.hff.mg>
2026-04-07 11:48:00 +0300

    refactor: lazy binding, support multi-formats identité, suppression debug

--- backend/src/Controller/DirectoryController.php
+++ backend/src/Controller/DirectoryController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 class DirectoryController extends AbstractController
 {
